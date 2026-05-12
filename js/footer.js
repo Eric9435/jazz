3f96f4e2793b636c929b1695370c710367dc027b
Eric9435 <aungphonemyatwytu@gmail.com>
2026-05-12 06:39:32 +0000

    Add global JS footer system

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+const footerHTML = `
+<footer class="footer">
+
+  <p>
+
+    <strong>
+      © 2026 Aung Phone Myat — Jazz Language Atlas™
+    </strong>
+
+    <br />
+
+    All Rights Reserved.
+
+  </p>
+
+</footer>
+`;
+
+document.body.insertAdjacentHTML(
+  "beforeend",
+  footerHTML
+);
